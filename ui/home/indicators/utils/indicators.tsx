5f7f0df51974461285f87859f1347b72a1f34dc4
Kenjii <kenjii@0xmakase.co.jp>
2024-07-30 20:32:44 +0900

    未修正箇所の日本語対応

--- ui/home/indicators/utils/indicators.tsx
+++ ui/home/indicators/utils/indicators.tsx
@@ -26,9 +26,7 @@ const dailyTxsIndicator: TChainIndicator<'stats_charts_txs'> = {
     'N/A' :
     Number(stats.transactions_today).toLocaleString(undefined, { maximumFractionDigits: 2, notation: 'compact' }),
   icon: <IconSvg name="transactions" boxSize={ 6 } bgColor="#56ACD1" borderRadius="base" color="white"/>,
-  hint: window.navigator.language === 'en' ?
-    `Number of transactions yesterday (0:00 - 23:59 UTC). The chart displays daily transactions for the past 30 days.` :
-    `昨日のトランザクション数（0:00 - 23:59 UTC）。グラフには過去30日間の1日あたりのトランザクション数が表示されます。`,
+  hint: 'indicatorsHint.dailyTxs',
   api: {
     resourceName: 'stats_charts_txs',
     dataFn: (response) => ([ {
@@ -51,9 +49,7 @@ const coinPriceIndicator: TChainIndicator<'stats_charts_market'> = {
     '$' + Number(stats.coin_price).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 6 }),
   valueDiff: (stats) => stats?.coin_price !== null ? stats?.coin_price_change_percentage : null,
   icon: <NativeTokenIcon boxSize={ 6 }/>,
-  hint: window.navigator.language === 'en' ?
-    `${ config.chain.currency.symbol } token daily price in USD.` :
-    `${ config.chain.currency.symbol } トークンの1日あたりの価格（USD）。`,
+  hint: 'indicatorsHint.coinPrice',
   api: {
     resourceName: 'stats_charts_market',
     dataFn: (response) => ([ {
@@ -76,9 +72,7 @@ const secondaryCoinPriceIndicator: TChainIndicator<'stats_charts_secondary_coin_
     '$' + Number(stats.secondary_coin_price).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 6 }),
   valueDiff: () => null,
   icon: <TokenLogoPlaceholder boxSize={ 6 }/>,
-  hint: window.navigator.language === 'en' ?
-    `${ config.chain.secondaryCoin.symbol } token daily price in USD.` :
-    `${ config.chain.secondaryCoin.symbol } トークンの1日あたりの価格（USD）。`,
+  hint: 'indicatorsHint.secondaryCoinPrice',
   api: {
     resourceName: 'stats_charts_secondary_coin_price',
     dataFn: (response) => ([ {
@@ -100,10 +94,7 @@ const marketPriceIndicator: TChainIndicator<'stats_charts_market'> = {
     '$N/A' :
     '$' + Number(stats.market_cap).toLocaleString(undefined, { maximumFractionDigits: 2, notation: 'compact' }),
   icon: <IconSvg name="globe" boxSize={ 6 } bgColor="#6A5DCC" borderRadius="base" color="white"/>,
-  hint: window.navigator.language === 'en' ?
-    'The total market value of a cryptocurrency\'s circulating supply.' +
-    'It is analogous to the free-float capitalization in the stock market. Market Cap = Current Price x Circulating Supply.' :
-    '暗号通貨の循環供給量の総市場価値。株式市場のフリーフロート時価総額に類似しています。時価総額 = 現在の価格 x 循環供給量。',
+  hint: 'indicatorsHint.marketCap',
   api: {
     resourceName: 'stats_charts_market',
     dataFn: (response) => ([ {
@@ -139,24 +130,20 @@ const tvlIndicator: TChainIndicator<'stats_charts_market'> = {
     '$N/A' :
     '$' + Number(stats.tvl).toLocaleString(undefined, { maximumFractionDigits: 2, notation: 'compact' }),
   icon: <IconSvg name="lock" boxSize={ 6 } bgColor="#517FDB" borderRadius="base" color="white"/>,
-  hint: window.navigator.language === 'en' ?
-    'Total value of digital assets locked or staked in a chain.' :
-    'チェーン内にロックまたはステークされたデジタル資産の総価値。',
+  hint: 'indicatorsHint.tvl',
   api: {
     resourceName: 'stats_charts_market',
     dataFn: (response) => ([ {
       items: response.chart_data
-        .map((item) => (
-          {
-            date: new Date(item.date),
-            value: item.tvl !== undefined ? item.tvl : 0,
-          }))
+        .map((item) => ({
+          date: new Date(item.date),
+          value: item.tvl !== undefined ? item.tvl : 0,
+        }))
         .sort(sortByDateDesc)
         .reduceRight(nonNullTailReducer, [] as Array<TimeChartItemRaw>)
         .map(mapNullToZero),
       name: 'TVL',
-      valueFormatter: (x: number) => '$' +
-      x.toLocaleString(undefined, { maximumFractionDigits: 2, notation: 'compact' }),
+      valueFormatter: (x: number) => '$' + x.toLocaleString(undefined, { maximumFractionDigits: 2, notation: 'compact' }),
     } ]),
   },
 };
