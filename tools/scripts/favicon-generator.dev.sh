170cff30849b091f28426d441e047bd5e38d78d4
Kenjii <kenjii@0xmakase.co.jp>
2024-07-31 14:53:31 +0900

    faviconを変更

--- tools/scripts/favicon-generator.dev.sh
+++ tools/scripts/favicon-generator.dev.sh
@@ -1,6 +1,6 @@
-secrets_file="./configs/envs/.env.secrets"
-favicon_folder="./public/assets/favicon/"
-master_url="https://raw.githubusercontent.com/blockscout/frontend/main/tools/scripts/favicon.svg"
+secrets_file="../../configs/envs/.env.secrets"
+favicon_folder="../../public/assets/favicon/"
+master_url="https://raw.githubusercontent.com/0xmakase/blockscout/master/apps/block_scout_web/assets/static/images/omakase-symbol.svg"
 
 if [ ! -f "$secrets_file" ]; then
     echo "Error: File '$secrets_file' not found."
@@ -10,10 +10,10 @@ fi
 dotenv \
   -v MASTER_URL=$master_url \
   -e $secrets_file \
-  -- bash -c 'cd ./deploy/tools/favicon-generator && ./script.sh'
+  -- bash -c 'cd ../../deploy/tools/favicon-generator && ./script.sh'
 
 if [ -d "$favicon_folder" ]; then
   rm -r "$favicon_folder"
 fi
 mkdir -p "$favicon_folder"
-cp -r ./deploy/tools/favicon-generator/output/* "$favicon_folder"
+cp -r ../../deploy/tools/favicon-generator/output/* "$favicon_folder"
