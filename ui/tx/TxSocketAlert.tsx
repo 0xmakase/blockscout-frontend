b5c2e2d018f8f665e4fe44e8751b737ccb75df59
Kenjii <kenjii@0xmakase.co.jp>
2024-07-31 11:28:02 +0900

    アラートメッセージの日本語化

--- ui/tx/TxSocketAlert.tsx
+++ ui/tx/TxSocketAlert.tsx
@@ -1,16 +1,18 @@
 import { Alert } from '@chakra-ui/react';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 interface Props {
   status: 'error' | 'close';
 }
 
 const TxSocketAlert = ({ status }: Props) => {
+  const { t } = useTranslation();
   const text = status === 'close' ?
-    'Connection is lost. Please click here to update transaction info.' :
-    'An error has occurred while fetching transaction info. Please click here to update.';
+    'blocksContent.connectionLostUpdate' :
+    'blocksContent.errorAlert';
 
-  return <Alert status="warning" as="a" href={ window.document.location.href }>{ text }</Alert>;
+  return <Alert status="warning" as="a" href={ window.document.location.href }>{ t(text) }</Alert>;
 };
 
 export default React.memo(TxSocketAlert);
